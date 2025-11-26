dccf082abe67e6ff1ad07306d31b00bc37656346
Rahmathsd <mdrahmathalisd@gmail.com>
2025-11-26 20:18:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -63,20 +63,3 @@ searchBtn.addEventListener('click', ()=>{
     checkWeather(inputBox.value);
 });
 
-
-// const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'x-rapidapi-key': '13dd16fb47msh8cc6dc8f1a8d157p1d84aejsn9dfc8a4c06b0',
-// 		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
-// 	}
-// };
-
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
